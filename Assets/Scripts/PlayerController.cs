970900be8644cee33ebcbb562197bdf217b1553d
cybernetchi <hi@kachi-chan.com>
2024-03-27 17:27:02 +0800

    Create Project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private GameObject focalPoint;
+    private float powerupStrength = 15.0f;
+
+    public float speed = 5.0f;
+    public bool hasPowerup;
+    public GameObject powerIndicator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("FocalPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+
+        powerIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+            powerIndicator.gameObject.SetActive(true);
+
+
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerIndicator.gameObject.SetActive(false);
+
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Debug.Log("Collided with" + collision.gameObject.name + "with powerup set to" + hasPowerup);
+
+            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            enemyRb.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+
+        }
+
+
+    }
+
+}
